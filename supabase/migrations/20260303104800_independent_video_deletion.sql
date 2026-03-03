9b69cde133609c31434c7914474b381c77b54f63
TALOU Films <taloufilms@TALOUs-MacBook-Pro.local>
2026-03-03 11:02:40 -0500

    db: change video deletion to SET NULL for independent removal

--- supabase/migrations/20260303104800_independent_video_deletion.sql
+++ supabase/migrations/20260303104800_independent_video_deletion.sql
@@ -1,4 +1,4 @@
--- SQL script to add ON DELETE CASCADE to video_generations.source_image_id foreign key
+-- SQL script to add ON DELETE SET NULL to video_generations.source_image_id foreign key
 
 -- Start a transaction safely
 BEGIN;
@@ -7,11 +7,11 @@ BEGIN;
 ALTER TABLE "public"."video_generations"
 DROP CONSTRAINT IF EXISTS "video_generations_source_image_id_fkey";
 
--- Add the foreign key constraint back with ON DELETE CASCADE
+-- Add the foreign key constraint back with ON DELETE SET NULL
 ALTER TABLE "public"."video_generations"
 ADD CONSTRAINT "video_generations_source_image_id_fkey"
 FOREIGN KEY ("source_image_id")
 REFERENCES "public"."prompt_generations"("id")
-ON DELETE CASCADE;
+ON DELETE SET NULL;
 
 COMMIT;
